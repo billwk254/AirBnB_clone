53bdc56237c66e6ae5273ecf9c5c00f7176efa50
billwk254 <billkihungu51@gmail.com>
2023-08-13 15:03:28 +0300

    Updated console.py

--- console.py
+++ console.py
@@ -37,94 +37,93 @@ class HBNBCommand(cmd.Cmd):
         """
         Create a new instance of BaseModel, saves it, and prints the id.
         """
-        args = line.split()
-        if not args:
+         args, n = parse(args)
+
+        if not n:
             print("** class name missing **")
-        elif args[0] not in storage.classes.keys():
+        elif args[0] not in classes:
             print("** class doesn't exist **")
+        elif n == 1:
+            temp = eval(args[0])()
+            print(temp.id)
+            temp.save()
         else:
-            new_instance = storage.classes[args[0]]()
-            new_instance.save()
-            print(new_instance.id)
+            print("** Too many arguments for create **")
 
     def do_show(self, line):
         """
         Prints the string representation of an instance.
         """
-        args = line.split()
-        if not args:
+        args, n = parse(arg)
+
+        if not n:
             print("** class name missing **")
-        elif args[0] not in storage.classes.keys():
-            print("** class doesn't exist **")
-        elif len(args) < 2:
+        elif n == 1:
             print("** instance id missing **")
-        else:
-            key = args[0] + "." + args[1]
-            if key in storage.all():
-                print(storage.all()[key])
-            else:
+        elif n == 2:
+            try:
+                inst = storage.find_by_id(*args)
+                print(inst)
+            except ModelNotFoundError:
+                print("** class doesn't exist **")
+            except InstanceNotFoundError:
                 print("** no instance found **")
+        else:
+            print("** Too many arguments for show **")
 
     def do_destroy(self, line):
         """
         Deletes an instance based on the class name and id.
         """
-        args = line.split()
-        if not args:
+        args, n = parse(arg)
+
+        if not n:
             print("** class name missing **")
-        elif args[0] not in storage.classes.keys():
-            print("** class doesn't exist **")
-        elif len(args) < 2:
+        elif n == 1:
             print("** instance id missing **")
-        else:
-            key = args[0] + "." + args[1]
-            if key in storage.all():
-                del storage.all()[key]
-                storage.save()
-            else:
+        elif n == 2:
+            try:
+                storage.delete_by_id(*args)
+            except ModelNotFoundError:
+                print("** class doesn't exist **")
+            except InstanceNotFoundError:
                 print("** no instance found **")
+        else:
+            print("** Too many arguments for destroy **")
 
     def do_all(self, line):
         """
         Prints all string representation of all instances.
         """
-        args = line.split()
-        obj_list = []
-        if not args:
-            for obj in storage.all().values():
-                obj_list.append(str(obj))
-            print(obj_list)
-        elif args[0] not in storage.classes.keys():
-            print("** class doesn't exist **")
+        args, n = parse(args)
+
+        if n < 2:
+            try:
+                print(storage.find_all(*args))
+            except ModelNotFoundError:
+                print("** class doesn't exist **")
         else:
-            for key, obj in storage.all().items():
-                if args[0] == key.split('.')[0]:
-                    obj_list.append(str(obj))
-            print(obj_list)
+            print("** Too many arguments for all **")
 
     def do_update(self, line):
         """
         Updates an instance based on the class name and id.
         """
-        args = line.split()
-        if not args:
+         args, n = parse(arg)
+        if not n:
             print("** class name missing **")
-        elif args[0] not in storage.classes.keys():
-            print("** class doesn't exist **")
-        elif len(args) < 2:
+        elif n == 1:
             print("** instance id missing **")
+        elif n == 2:
+            print("** attribute name missing **")
+        elif n == 3:
+            print("** value missing **")
         else:
-            key = args[0] + "." + args[1]
-            if key in storage.all():
-                if len(args) < 3:
-                    print("** attribute name missing **")
-                elif len(args) < 4:
-                    print("** value missing **")
-                else:
-                    obj = storage.all()[key]
-                    setattr(obj, args[2], args[3])
-                    obj.save()
-            else:
+            try:
+                storage.update_one(*args[0:4])
+            except ModelNotFoundError:
+                print("** class doesn't exist **")
+            except InstanceNotFoundError:
                 print("** no instance found **")
 
 
