92226031700dd4bbbcb4443735ebbeb5d2f94f35
billwk254 <billkihungu51@gmail.com>
2023-08-13 14:50:22 +0300

    Reverted back to previous commit

--- console.py
+++ console.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-
-
-import cmd
-from models import storage
-from models.base_model import BaseModel
-
-
 """
 Module for the command interpreter.
 """
+
+import cmd
 
 
 class HBNBCommand(cmd.Cmd):
@@ -35,38 +30,6 @@ class HBNBCommand(cmd.Cmd):
         Do nothing on empty line.
         """
         pass
-
-    def do_create(self, line):
-        """
-        Create a new instance of BaseModel, saves it, and prints the id.
-        """
-        args = line.split()
-        if not args:
-            print("** class name missing **")
-        elif args[0] not in storage.classes.keys():
-            print("** class doesn't exist **")
-        else:
-            new_instance = storage.classes[args[0]]()
-            new_instance.save()
-            print(new_instance.id)
-
-    def do_show(self, line):
-        """
-        Prints the string representation of an instance.
-        """
-        args = line.split()
-        if not args:
-            print("** class name missing **")
-        elif args[0] not in storage.classes.keys():
-            print("** class doesn't exist **")
-        elif len(args) < 2:
-            print("** instance id missing **")
-        else:
-            key = args[0] + "." + args[1]
-            if key in storage.all():
-                print(storage.all()[key])
-            else:
-                print("** no instance found **")
 
 
 if __name__ == '__main__':
