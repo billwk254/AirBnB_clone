d1ee7e8c9890a840f2a2ef79133761daf1b649cd
billwk254 <billkihungu51@gmail.com>
2023-08-13 17:41:24 +0300

    Reverted console.py to first commit

--- console.py
+++ console.py
@@ -4,11 +4,7 @@ Module for the command interpreter.
 """
 
 
-from models import storage
-from models.base_model import BaseModel
-from cmd import Cmd
-
-model_classes = storage.models
+import cmd
 
 
 class HBNBCommand(cmd.Cmd):
@@ -35,89 +31,6 @@ class HBNBCommand(cmd.Cmd):
         Do nothing on empty line.
         """
         pass
-
-    def do_create(self, line):
-        """Creates a new instance of a model"""
-        parsed_args, arg_count = parse(args)
-
-        if not arg_count:
-            print("** class name missing **")
-        elif parsed_args[0] not in custom_classes:
-            print("** class doesn't exist **")
-        elif arg_count == 1:
-            new_instance = eval(parsed_args[0])()
-            print(new_instance.id)
-            new_instance.save()
-        else:
-            print("** Too many arguments for create **")
-
-    def do_show(self, line):
-        """Shows an instance based on its class name and id"""
-        parsed_args, arg_count = parse(arg)
-
-        if not arg_count:
-            print("** class name missing **")
-        elif arg_count == 1:
-            print("** instance id missing **")
-        elif arg_count == 2:
-            try:
-                instance = storage.find_by_id(*parsed_args)
-                print(instance)
-            except ModelNotFoundError:
-                print("** class doesn't exist **")
-            except InstanceNotFoundError:
-                print("** no instance found **")
-        else:
-            print("** Too many arguments for show **")
-
-    def do_destroy(self, line):
-        """Destroys an instance based on its class name and id"""
-        parsed_args, arg_count = parse(arg)
-
-        if not arg_count:
-            print("** class name missing **")
-        elif arg_count == 1:
-            print("** instance id missing **")
-        elif arg_count == 2:
-            try:
-                storage.delete_by_id(*parsed_args)
-            except ModelNotFoundError:
-                print("** class doesn't exist **")
-            except InstanceNotFoundError:
-                print("** no instance found **")
-        else:
-            print("** Too many arguments for destroy **")
-
-    def do_all(self, line):
-        """Prints all instances of a specific class"""
-        parsed_args, arg_count = parse(args)
-
-        if arg_count < 2:
-            try:
-                print(storage.find_all(*parsed_args))
-            except ModelNotFoundError:
-                print("** class doesn't exist **")
-        else:
-            print("** Too many arguments for all **")
-
-    def do_update(self, line):
-        """Updates an instance based on its class name, id, attribute name, and value"""
-        parsed_args, arg_count = parse(arg)
-        if not arg_count:
-            print("** class name missing **")
-        elif arg_count == 1:
-            print("** instance id missing **")
-        elif arg_count == 2:
-            print("** attribute name missing **")
-        elif arg_count == 3:
-            print("** value missing **")
-        else:
-            try:
-                storage.update_one(*parsed_args[0:4])
-            except ModelNotFoundError:
-                print("** class doesn't exist **")
-            except InstanceNotFoundError:
-                print("** no instance found **")
 
 
 if __name__ == '__main__':
