447adaea7eaceb5a5638f1d032bb53859b0e4e1e
billwk254 <billkihungu51@gmail.com>
2023-08-13 14:01:30 +0300

    Updated __init__ files

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,3 @@
-#!/usr/bin/python3
-"""
-Initializes the package.
-"""
+from models.engine import storage
 
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
+__all__ = ["BaseModel"]
