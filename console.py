21782321a0fa0d0c63a609bdc39981912a045322
billwk254 <billkihungu51@gmail.com>
2023-08-13 14:13:27 +0300

    console.py that contains the entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module for the command interpreter.
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    The command interpreter class.
+    """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """
+        Quit command to exit the program.
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+        Exit the program with EOF (Ctrl-D).
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Do nothing on empty line.
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
