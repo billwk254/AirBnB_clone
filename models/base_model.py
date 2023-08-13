f2a46843844ccb86b43890266fb2bc1afdbe1d9a
billwk254 <billkihungu51@gmail.com>
2023-08-13 13:31:14 +0300

    Updated BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
+
 import uuid
 from datetime import datetime
 
 
-"""Defines the BaseModel class."""
+"""BaseModel class for the project"""
 
 
 class BaseModel:
@@ -13,21 +15,24 @@ class BaseModel:
         id (str): The ID of the instance.
         created_at (datetime): The creation date and time.
         updated_at (datetime): The last update date and time.
-    """    
-    def __init__(self):
+    """
+
+    def __init__(self, *args, **kwargs):
         """Initialize BaseModel attributes."""
         if kwargs:
-            if "__class__" in kwargs:
-                del kwargs["__class__"]
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+
+    def __str__(self):
+        """Return the string representation of BaseModel."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Update the updated_at attribute with the current datetime."""
@@ -40,7 +45,3 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-    
-    def __str__(self):
-        """Return the string representation of BaseModel."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
