bac0cb246a15c605fcf54e5a91d7193cfbd299ff
billwk254 <billkihungu51@gmail.com>
2023-08-13 14:10:01 +0300

    Updated filestorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,31 +17,29 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary __objects."""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """Sets in __objects the obj with key"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)."""
-        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            json.dump(obj_dict, f)
+        """Serializes __objects to the JSON file"""
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects."""
         try:
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-                obj_dict = json.load(f)
-                for key, value in obj_dict.items():
-                    cls_name = value["__class__"]
-                    cls = models.get_class(cls_name)
-                    FileStorage.__objects[key] = cls(**value)
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split(".")
+                    class_ = eval(class_name)
+                    self.__objects[key] = class_(**value)
         except FileNotFoundError:
             pass
-
-
-storage = FileStorage()
-storage.reload()
