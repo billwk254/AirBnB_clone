c7924abd99fd17b0ae284f493cb02af274cb1f12
billwk254 <billkihungu51@gmail.com>
2023-08-14 14:57:07 +0300

    Updated code for the save function in file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,13 +32,13 @@ class FileStorage:
         key = obj.__class__.__name__ + "." + obj.id
         self.__objects[key] = obj
 
-        def save(self):
-            """Serializes __objects to the JSON file"""
-            serialized_objects = {}
-            for key, obj in self.__objects.items():
-                serialized_objects[key] = obj.to_dict()
-            with open(self.__file_path, 'w') as file:
-                json.dump(serialized_objects, file)
+    def save(self):
+        """Serializes __objects to the JSON file"""
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects."""
