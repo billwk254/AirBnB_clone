ee2d9a01799fa9344d3a692e6c6159f52aa9c3c3
billwk254 <billkihungu51@gmail.com>
2023-08-14 15:42:14 +0300

    All other classes that inherit from BaseModel

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines the State class
+"""
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class that inherits from BaseModel
+    """
+
+    name = ""
